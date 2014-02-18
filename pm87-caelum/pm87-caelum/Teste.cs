070c8d81e362e658778440d5de8cc9505f4a7751
kanashiro <willianatsuki@gmail.com>
2014-02-18 19:30:23 -0300

    arrumando o projeto de teste.

---
+++ pm87-caelum/pm87-caelum/Teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pm87_caelum
+{
+    public class Teste
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de teste de uso git.");
+        }
+    }
+}
