3fa47f49c66d6679eb359b761dd9200093df88c4
kanashiro <willianatsuki@gmail.com>
2014-02-18 21:04:44 -0300

    mudando arquivo do teste

--- pm87-caelum/pm87-caelum/Teste.cs
+++ pm87-caelum/pm87-caelum/Teste.cs
@@ -8,6 +8,7 @@ namespace pm87_caelum
 {
     public class Teste
     {
+        // guloso
         public static void Main(string[] args)
         {
             Console.WriteLine("Exemplo de teste de uso git.");
